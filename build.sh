6d6ba8d603f5e59844d3976f0b3554889afda463
xiexueni <xiexueni@xbank.plus>
2023-06-09 12:48:34 +0800

    fix: build sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 # !/bin/bash
-echo $CF_PAGES_BRANCH == "master"
-if [ $CF_PAGES_BRANCH == "master" ]; then  # Run the "production" script in `package.json` on the "production" branch  # "production" should be replaced with the name of your Production branch
+if [ "$CF_PAGES_BRANCH" == "master" ]; then
+  # Run the "production" script in `package.json` on the "production" branch
+  # "production" should be replaced with the name of your Production branch
+
   npm run build:production
 
-else  # Else run the dev script  npm run devfi
+else
+  # Else run the dev script
   npm run build:staging
 
 fi
