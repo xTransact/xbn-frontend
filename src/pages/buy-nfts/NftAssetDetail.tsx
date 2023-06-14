7c0d3cd1150a90bacf09a86d6830b678a2a27111
xiexueni <xiexueni@xbank.plus>
2023-06-14 17:20:05 +0800

    fix: loan step loading

--- src/pages/buy-nfts/NftAssetDetail.tsx
+++ src/pages/buy-nfts/NftAssetDetail.tsx
@@ -550,11 +550,14 @@ const NftAssetDetail = () => {
 
         // 如果一直监听不到
         timer.current = setTimeout(() => {
-          toast({
-            status: 'info',
-            title: 'The loan is being generated, please wait and refresh later',
-          })
-          navigate('/buy-nfts/loans')
+          if (loanStep === 'loading') {
+            toast({
+              status: 'info',
+              title:
+                'The loan is being generated, please wait and refresh later',
+            })
+            navigate('/buy-nfts/loans')
+          }
         }, 10 * 60 * 1000)
       } catch (error: any) {
         toastError(error)
@@ -577,6 +580,7 @@ const NftAssetDetail = () => {
     commodityWeiPrice,
     interceptFn,
     toast,
+    loanStep,
     platform,
   ])
 
